d5e9468b3a0882242fe30429734ac9402b28d78f
Archie Baer <archie@abaer.dev>
2020-07-30 15:52:40 +0100

    API

---
+++ pages/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default async (req, res) => {
+    try {
+        if (typeof req.query.id === "string") {
+            const user = (await axios.get(`https://horizon.alles.cc/users/${encodeURIComponent(req.query.id)}`)).data;
+            res.json({text: `${user.name}#${user.tag}`});
+        } else if (
+            typeof req.query.name === "string" &&
+            typeof req.query.tag === "string"
+        ) {
+            const user = (await axios.get(
+                `https://horizon.alles.cc/nametag?name=${encodeURIComponent(req.query.name)}&tag=${encodeURIComponent(req.query.tag)}`
+            )).data;
+            res.json({text: user.id});
+        } else res.status(400).json({err: "badRequest"});
+    } catch (err) {
+        res.status(400).json({err: "missingResource"});
+    }
+};
